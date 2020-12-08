9374cd642ae6c1fc69d58d62f30999b135916412
xukele <giliguala1024@qq.com>
2020-12-08 17:28:32 +0800

    全局异常处理2

--- kele-service-common/src/main/java/com/kele/aggregation/common/config/GlobalExceptionHandleConfig.java
+++ kele-service-common/src/main/java/com/kele/aggregation/common/config/GlobalExceptionHandleConfig.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import static com.kele.aggregation.common.dto.KeleResult.FAIL_CODE_UNKNOWN;
+
 @Slf4j
 /**
  * 需自行注册处理 @RestControllerAdvice
@@ -18,5 +20,13 @@ public class GlobalExceptionHandleConfig {
     public KeleResult<Object> handleBizRuntimeException(BizRuntimeException ex) {
         log.warn("[exception record, code={},message=]", ex.getCode(), ex);
         return KeleResult.fail(ex.getCode(), ex.getMessage());
+    }
+
+
+    @ResponseStatus(HttpStatus.OK)
+    @ExceptionHandler(Exception.class)
+    public KeleResult<Object> handleException(Exception ex) {
+        log.warn("[exception record, code={},message=]", FAIL_CODE_UNKNOWN, ex);
+        return KeleResult.fail(FAIL_CODE_UNKNOWN, "系统异常");
     }
 }
